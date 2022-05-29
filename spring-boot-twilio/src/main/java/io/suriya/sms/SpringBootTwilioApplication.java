9b4621556b8448277e30ba5f55a14311ebf078cd
Suriya R <kartsuri89@gmail.com>
2022-05-30 00:13:19 +0530

    Initial spring boot twilio marry

---
+++ spring-boot-twilio/src/main/java/io/suriya/sms/SpringBootTwilioApplication.java
@@ -0,0 +1,13 @@
+package io.suriya.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwilioApplication.class, args);
+	}
+
+}
---
+++ spring-boot-twilio/src/test/java/io/suriya/sms/SpringBootTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suriya.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTwilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
